78b950fa1ac8fa65370a2b99e39b18d0b57faee6
Sara Salzano <sarasalzano95@gmail.com>
2025-09-03 15:03:31 +0200

    esegui esercizio 03

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -1,13 +1,24 @@
 /* Scrivi una funzione che accetti un'array di stringhe e una lettera e restituisca un array contenente solo le parole che iniziano con quella lettera */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
+const namesStartingWithA = [];
+let letterA = "a"
 
 
 // Dichiara la funzione qui.
 
+function selectLetter(array1, array2) {
+    for(let i=0; i<array1.length; i++){
+        if(letterA.toLowerCase() === array1[i][0].toLowerCase()){
+        array2.push(array1[i]);
+        }
+    }
+    return array2;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+selectLetter(names, namesStartingWithA);
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
+console.log(selectLetter(names, namesStartingWithA));
