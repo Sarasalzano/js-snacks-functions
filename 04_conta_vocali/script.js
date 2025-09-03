bd5791e810404915d7c37a7b78af0178033bfba7
Sara Salzano <sarasalzano95@gmail.com>
2025-09-03 17:47:43 +0200

    commenti esercizio 04

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -1,15 +1,23 @@
 /* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
 
 const word = 'javascript';
+//scrivo array contenenti vocali
 const vowelsArray = ["a", "e", "i", "o", "u"];
+
+//creo variabile di stato da utilizzare per chiedere se la vocale è stata trovata
 let isItaVowel = false;
 
 // Dichiara la funzione qui.
 function vowelsCheck(nome) {
+    //creo variabile vuota in cui andranno le vocali
     let vowelsOnly = "";
+    //creo un ciclo che parte da 0 e continua fin quando il contatore è minore della lunghezza del nome
     for(let i=0; i<nome.length; i++){
+    /*verifico se nella variabile nome esistono le vocali presenti nel vowelsArray. Per verificare l'esistenza scrivo !==-1 poiché .indexOf da -1 per indicare l'assenza di un elemento */
     if(vowelsArray.indexOf(nome[i]) !==-1){
+    //se le vocali sono presenti allora la variabile è vera
     isItaVowel=true;
+    //aggiungo gli elementi (le vocali) nella la variabile vowelsOnly 
     vowelsOnly += nome[i];
     }
     }
